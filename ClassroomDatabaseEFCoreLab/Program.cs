af9e8913128a1d8dc867afa065f7ddeab39ffbeb
Troy Wilson <Troy.Wilson.Anthony@gmail.com>
2022-09-10 11:48:42 -0400

    Added CreateDB method

--- ClassroomDatabaseEFCoreLab/Program.cs
+++ ClassroomDatabaseEFCoreLab/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ClassroomDatabaseEFCoreLab
+﻿using System.Xml.Linq;
+
+namespace ClassroomDatabaseEFCoreLab
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            //CreateDB();
+
+
+
+
+
+
+
+
+
+            static void CreateDB()
+            {
+                using(var context = new ClassContext())
+                {
+                    var students = new List<Student> {
+                         new Student { Name = "Troy", Food = "Pizza", Hometown = "Detroit" },
+                         new Student { Name = "Tom", Food = "Steak", Hometown = "Coral" },
+                         new Student { Name = "Jude", Food = "Chicken", Hometown = "Grand Rapids" },
+                    };
+
+                    foreach(var student in students)
+                    {
+                        context.Students.Add(student);
+                    }
+                    context.SaveChanges();
+
+                    //for demo purposes
+                    var allStudents = context.Students.ToList();
+                    Console.WriteLine($"Current student count: {allStudents.Count}");
+                }
+            }
         }
     }
 }
