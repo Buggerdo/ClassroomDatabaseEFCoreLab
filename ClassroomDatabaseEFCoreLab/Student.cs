1efc7b771cf47db33912f7f848d655f597da6c31
Troy Wilson <Troy.Wilson.Anthony@gmail.com>
2022-09-10 11:10:55 -0400

    Added student class

---
+++ ClassroomDatabaseEFCoreLab/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassroomDatabaseEFCoreLab
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Food { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Hometown { get; set; }
+    }
+}
