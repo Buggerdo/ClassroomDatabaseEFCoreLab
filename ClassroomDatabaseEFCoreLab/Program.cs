9272db2031a1a76ef700763dc6b8f48d4ecf15d9
Troy Wilson <Troy.Wilson.Anthony@gmail.com>
2022-09-10 12:17:40 -0400

    added comment

--- ClassroomDatabaseEFCoreLab/Program.cs
+++ ClassroomDatabaseEFCoreLab/Program.cs
@@ -14,12 +14,21 @@ namespace ClassroomDatabaseEFCoreLab
 
 
 
-
-
+            //In your program file, create a method called CreateDB. 
+            //This should create 1 new student for everyone in the class. 
+            //Add their name, favorite food and hometown to the student.
+            //Once all students have been added, save the changes to the DB.
+            //Run this method once, then comment it out. We only want it to fill the db
             static void CreateDB()
             {
                 using(var context = new ClassContext())
                 {
+                    var allStudents = context.Students.ToList();
+
+                    //Extra credit
+                    //Make your code run CreateDB if the database is empty
+                    if(allStudents.Count == 0)
+                    {
                     var students = new List<Student> {
                          new Student { Name = "Troy", Food = "Pizza", Hometown = "Detroit" },
                          new Student { Name = "Tom", Food = "Steak", Hometown = "Coral" },
