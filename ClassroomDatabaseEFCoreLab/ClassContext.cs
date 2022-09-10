029e9769334438f1391c399fbac62dec8694e297
Troy Wilson <Troy.Wilson.Anthony@gmail.com>
2022-09-10 11:11:23 -0400

    Added classContext class

---
+++ ClassroomDatabaseEFCoreLab/ClassContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClassroomDatabaseEFCoreLab
+{
+    public class ClassContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=TROYPC;Database=CodeFirstExample;Integrated Security=SSPI;");
+        }
+    }
+}
